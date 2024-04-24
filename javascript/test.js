f9150bbc8b50716a46bb7735a61a0012e8f3e926
timasssh <timmylbueno@gmail.com>
2024-04-24 02:54:59 -0300

    creation of an test.js file that will test the functions of this folder

---
+++ javascript/test.js
@@ -0,0 +1,6 @@
+const narcissistic = require("./narcisistic_number");
+
+// Expected: true, true, false
+console.log(narcissistic(7));
+console.log(narcissistic(153));
+console.log(narcissistic(100));
